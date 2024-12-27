3cc30aa0bcde1eb561e3b6b276496233d45e148f
stefffffffffano <s323195@studenti.polito.it>
2024-12-27 12:06:16 +0100

    Added splitting mantaining distribution among classes through train_test_split

--- data/cifar100/cifar100_loader.py
+++ data/cifar100/cifar100_loader.py
@@ -1,6 +1,7 @@
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import train_test_split
 
 def load_cifar100(batch_size=32, validation_split=0.1, download=True):
     transform = transforms.Compose([
@@ -11,19 +12,16 @@ def load_cifar100(batch_size=32, validation_split=0.1, download=True):
     # Load the full training set
     full_trainset = datasets.CIFAR100(root='./data', train=True, download=download, transform=transform)
 
-    # Split the training set into training and validation sets
-    num_train = len(full_trainset)
-    num_valid = int(num_train * validation_split)
-    num_train -= num_valid
+    indexes = range(0, len(full_trainset))
+    splitting = train_test_split(indexes, train_size = 1-validation_split, random_state = 42, stratify = full_trainset.data["label"], shuffle = True)
+    train_indexes = splitting[0]
+    val_indexes = splitting[1]
 
-    #Use the function used during lab 1 to split the dataset coherently
-    trainset, validset = random_split(full_trainset, [num_train, num_valid])
-
-    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-    validloader = DataLoader(validset, batch_size=batch_size, shuffle=True)
+    train_dataset = Subset(full_trainset, train_indexes)
+    val_dataset = Subset(full_trainset, val_indexes)
 
     # Load the test dataset
     testset = datasets.CIFAR100(root='./data', train=False, download=download, transform=transform)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-    return trainloader, validloader, testloader
+    return train_dataset, val_dataset, testloader
