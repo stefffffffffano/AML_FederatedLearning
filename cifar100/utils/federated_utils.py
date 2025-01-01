90c7b55101cd00038c40d0c2ba79dc1a98b70700
Luca Dadone <luca.dadone01@gmail.com>
2025-01-01 19:13:51 +0100

    Important fix: Improve evaluation approach and reduce training round duration. Updated the client update function to compute training loss and accuracy on-the-fly, eliminating the need to re-evaluate the training set after each round. This adjustment aligns the evaluation approach with centralized training practices, significantly reducing the duration of a training round from ~20 seconds to ~3.3 seconds.

--- cifar100/utils/federated_utils.py
+++ cifar100/utils/federated_utils.py
@@ -105,9 +105,10 @@ def client_selection(number_of_clients, clients_fraction, gamma=None):
     return selected_clients
    
 
-def client_update(model, client_id, client_data, criterion, optimizer, local_steps=4, detailed_print=False):
-    """
-    Trains a given client's local model on its dataset for a fixed number of steps (`local_steps`).
+def client_update(model, client_id, client_data, criterion, optimizer, local_steps=4, print_log=False):
+    """Client Update Function for Federated Learning
+
+    This function trains a client's local model using their dataset for a fixed number of steps (`local_steps`). The function handles dynamic batch sizes and calculates loss and accuracy with precision to ensure robustness, especially in cases where the dataset size is not divisible by the batch size.
 
     Args:
         model (nn.Module): The local model to be updated.
@@ -115,25 +116,26 @@ def client_update(model, client_id, client_data, criterion, optimizer, local_ste
         client_data (DataLoader): The data loader for the client's dataset.
         criterion (Loss): The loss function used for training (e.g., CrossEntropyLoss).
         optimizer (Optimizer): The optimizer used for updating model parameters (e.g., SGD).
-        local_steps (int): Number of local epochs to train on the client's dataset.
-        detailed_print (bool): If True, logs the final loss after training.
+        local_steps (int): Number of local steps (batches) to train on the client's dataset.
+        print_log (bool): If True, logs the final loss and accuracy after training.
 
     Returns:
-        dict: The state dictionary of the updated model.
+        tuple: A tuple containing the updated model's state dictionary, the averaged loss, and the accuracy.
     """
-
 
     cudnn.benchmark  # Calling this optimizes runtime
 
     model.train()  # Set the model to training mode
+
     step_count = 0
+    total_loss = 0.0
+    correct_predictions = 0
+    total_samples = 0
+
     while step_count < local_steps:
         for data, targets in client_data:
             # Move data and targets to the specified device (e.g., GPU or CPU)
             data, targets = data.to(DEVICE), targets.to(DEVICE)
-
-
-            start_time = time.time()  # for testing-----------------------------
 
             # Reset the gradients before backpropagation
             optimizer.zero_grad()
@@ -148,23 +150,29 @@ def client_update(model, client_id, client_data, criterion, optimizer, local_ste
             loss.backward()
             optimizer.step()
 
-            # for testing ------------------------------------------------------
-            if detailed_print:
-              end_time = time.time()  # Record the end time
-              elapsed_time = end_time - start_time  # Calculate the elapsed time
-              print(f'Single step time taken: {elapsed_time:.4f} seconds')
+            #---------- Accumulate metrics
+            #  Accumulates the weighted loss for the number of samples in the batch to account for any variation in 
+            #  batch size due to, for example, the smaller final batch. A little too precise? :) 
+            total_loss += loss.item() * data.size(0)
+            _, predicted = outputs.max(1)
+            correct_predictions += predicted.eq(targets).sum().item()
+            total_samples += data.size(0)
 
             step_count +=1
             if step_count >= local_steps:
               break
+    
+    #---------- Compute averaged metrics
+    avg_loss = total_loss / total_samples
+    avg_accuracy = correct_predictions / total_samples * 100
 
     # Optionally, print the loss for the last epoch of training
-    if detailed_print:
-        print(f'Client {client_id} --> Final Loss (Step {step_count}/{local_steps}): {loss.item()}')
+    if print_log:
+      print(f'Client {client_id} --> Final Loss (Step {step_count}/{local_steps}): {loss.item()}')
 
 
-    # Return the updated model's state dictionary (weights)
-    return model.state_dict()
+    # Return the updated model's state dictionary (weights) and mentrics
+    return model.state_dict(), avg_loss, avg_accuracy
 
 def evaluate(model, dataloader):
     with torch.no_grad():
@@ -183,23 +191,25 @@ def evaluate(model, dataloader):
             # Update Corrects
             running_corrects += torch.sum(preds == targets.data).data.item()
             # Calculate Accuracy
-            accuracy = running_corrects / float(len(dataloader.dataset))
+            accuracy = running_corrects / float(len(dataloader.dataset)) * 100
 
     return accuracy, mean(losses)
 
 
-def fedavg_aggregate(global_model, client_states, client_sizes):
+def fedavg_aggregate(global_model, client_states, client_sizes, client_avg_losses, client_avg_accuracies):
     """
-    Aggregates model updates from selected clients using the Federated Averaging (FedAvg) algorithm.
-    The updates are weighted by the size of each client's dataset.
+    Aggregates model updates and client metrics from selected clients using the Federated Averaging (FedAvg) algorithm.
+    The updates and metrics are weighted by the size of each client's dataset.
 
     Args:
         global_model (nn.Module): The global model whose structure is used for aggregation.
         client_states (list[dict]): A list of state dictionaries (model weights) from participating clients.
         client_sizes (list[int]): A list of dataset sizes for the participating clients.
+        client_avg_losses (list[float]): A list of average losses for the participating clients.
+        client_avg_accuracies (list[float]): A list of average accuracies for the participating clients.
 
     Returns:
-        dict: The aggregated state dictionary with updated model parameters.
+        tuple: The aggregated state dictionary with updated model parameters, global average loss, and global average accuracy.
     """
     # Copy the global model's state dictionary for aggregation
     new_state = deepcopy(global_model.state_dict())
@@ -211,15 +221,24 @@ def fedavg_aggregate(global_model, client_states, client_sizes):
     for key in new_state:
         new_state[key] = torch.zeros_like(new_state[key])
 
-    # Perform a weighted average of client updates
-    for state, size in zip(client_states, client_sizes):
+    # Initialize metrics
+    total_loss = 0.0
+    total_accuracy = 0.0
+
+    # Perform a weighted average of client updates and metrics
+    for state, size, avg_loss, avg_accuracy in zip(client_states, client_sizes, client_avg_losses, client_avg_accuracies):
         for key in new_state:
             # Add the weighted contribution of each client's parameters
             new_state[key] += (state[key] * size / total_samples)
+        total_loss += avg_loss * size
+        total_accuracy += avg_accuracy * size
 
-    # Return the aggregated state dictionary with updated weights
-    return new_state
+    # Calculate global metrics
+    global_avg_loss = total_loss / total_samples
+    global_avg_accuracy = total_accuracy / total_samples
 
+    # Return the aggregated state dictionary with updated weights and global metrics
+    return new_state, global_avg_loss, global_avg_accuracy
 
 import time
 
@@ -247,6 +266,7 @@ def train_federated(global_model, criterion, trainloader, validloader, num_clien
       train_losses = data_to_load['train_losses']
       client_selection_count = data_to_load['client_selection_count']
 
+
     # ********************* HOW IT WORKS ***************************************
     # The training runs for rounds iterations (GLOBAL_ROUNDS=2000)
     # Each round simulates one communication step in federated learning, including:
@@ -263,39 +283,45 @@ def train_federated(global_model, criterion, trainloader, validloader, num_clien
         #     This reduces computation costs and mimics real-world scenarios where not all devices are active.
         selected_clients = random.sample(range(num_clients), int(C * num_clients))
         client_states = []
+        client_avg_losses = []
+        client_avg_accuracies = []
+
         for client_id in selected_clients:
             client_selection_count[client_id] += 1
 
-        # 2) local training: for each client updates the model using the client's data for local_steps epochs
+        # 2) local training: for each client updates the model using the client's data for local_steps steps
         for client_id in selected_clients:
             local_model = deepcopy(global_model) #it creates a local copy of the global model
             optimizer = optim.SGD(local_model.parameters(), lr=lr, momentum=momentum, weight_decay=wd) #same of the centralized version
             client_loader = DataLoader(shards[client_id], batch_size=batchsize, shuffle=True)
 
-            local_state = client_update(local_model, client_id, client_loader, criterion, optimizer, local_steps, round_num % log_freq == 0 and detailed_print)
-            client_states.append(local_state)
+            print_log = round_num % log_freq == 0 and detailed_print
+            client_local_state, client_avg_loss, client_avg_accuracy  = client_update(local_model, client_id, client_loader, criterion, optimizer, local_steps, print_log)
+            
+            client_states.append(client_local_state)
+            client_avg_losses.append(client_avg_loss)
+            client_avg_accuracies.append(client_avg_accuracy)
 
-        
         # 3) central aggregation: aggregates participating client updates using fedavg_aggregate
         #    and replaces the current parameters of global_model with the returned ones.
-        global_model.load_state_dict(fedavg_aggregate(global_model, client_states, [client_sizes[i] for i in selected_clients]))
+        aggregated_state, train_loss, train_accuracy = fedavg_aggregate(global_model, client_states, [client_sizes[i] for i in selected_clients], client_avg_losses, client_avg_accuracies)
+        global_model.load_state_dict(aggregated_state)    
 
+        train_accuracies.append(train_accuracy)
+        train_losses.append(train_loss)
+        
         #Validation at the server
-        #if round_num % log_freq:
         val_accuracy, val_loss = evaluate(global_model, validloader)
         val_accuracies.append(val_accuracy)
         val_losses.append(val_loss)
+
         if val_accuracy > best_val_acc:
             best_val_acc = val_accuracy
             best_model_state = deepcopy(global_model.state_dict())
 
         if round_num % log_freq == 0:
-            train_accuracy, train_loss = evaluate(global_model, trainloader)
-            train_accuracies.append(train_accuracy)
-            train_losses.append(train_loss)
-
-            print(f"--> best validation accuracy: {best_val_acc}\n--> training accuracy: {train_accuracy}")
-            print(f"--> validation loss: {val_loss}\n--> training loss: {train_loss}")
+            print(f"--> best validation accuracy: {best_val_acc:.2f}%\n--> training accuracy: {train_accuracy:.2f}%")
+            print(f"--> validation loss: {val_loss:.4f}\n--> training loss: {train_loss:.4fs}")
 
             # checkpointing
             checkpoint_data = {
@@ -319,7 +345,6 @@ def train_federated(global_model, criterion, trainloader, validloader, num_clien
     global_model.load_state_dict(best_model_state)
 
     return global_model, val_accuracies, val_losses, train_accuracies, train_losses, client_selection_count
-
 
 def plot_client_selection(client_selection_count, file_name):
     """
